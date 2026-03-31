6303c10dd2f5dd099ffa65f002fba974d88067f2
Julia-Abramova <yulia.abramova14@yandex.ru>
2026-03-31 10:27:37 +0300

    абстрактный класс-декоратор

---
+++ Task2/src/main/java/org/example/nordicStew/AdditiveDecorator.java
@@ -0,0 +1,19 @@
+package org.example.nordicStew;
+
+public abstract class AdditiveDecorator implements Dish {
+    protected Dish wrappedDish;
+
+    public AdditiveDecorator(Dish dish) {
+        this.wrappedDish = dish;
+    }
+
+    @Override
+    public String getDescription() {
+        return wrappedDish.getDescription();
+    }
+
+    @Override
+    public int getPrice() {
+        return wrappedDish.getPrice();
+    }
+}
